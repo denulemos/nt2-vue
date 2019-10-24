b14ba8904193a67b68b32b9c6c077fd11a82649b
juan <juanpablomota96@hotmail.com>
2019-10-23 21:37:50 -0300

    nueva pantalla de calendario en paciente y medico

--- src/components/medico.vue
+++ src/components/medico.vue
@@ -2,7 +2,6 @@
     <div id="app">
   <v-app id="inspire">
     <v-container fluid>
-     
           <v-sheet
             elevation="12"
             class="pa-12"
@@ -25,9 +24,70 @@
               :counter="counterEn ? counter : false"
               :dense="dense"
             ></v-text-field>
-            
+                     <v-row justify="center">
+    <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+      <template v-slot:activator="{ on }">
+        <v-btn depressed large color="primary" dark v-on="on">Ver Turnos</v-btn>
+      </template>
+      <v-card>
+        <v-toolbar dark color="primary">
+          <v-btn icon dark @click="dialog = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+          <v-toolbar-title>Próximos Turnos</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-toolbar-items>
+            <v-btn dark text @click="dialog = false">Save</v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
+        <v-list three-line subheader>
+           <v-row>
+    <v-col>
+      <v-sheet height="400">
+        <v-calendar
+          ref="calendar"
+          :now="today"
+          :value="today"
+          :events="events"
+          color="primary"
+          type="week"
+        >
+          <!-- the events at the top (all-day) -->
+          <template v-slot:day-header="{ date }">
+            <template v-for="event in eventsMap[date]">
+              <!-- all day events don't have time -->
+              <div
+                v-if="!event.time"
+                :key="event.title"
+                class="my-event"
+                @click="open(event)"
+                v-html="event.title"
+              ></div>
+            </template>
+          </template>
+          <!-- the events at the bottom (timed) -->
+          <template v-slot:day-body="{ date, timeToY, minutesToPixels }">
+            <template v-for="event in eventsMap[date]">
+              <!-- timed events -->
+              <div
+                v-if="event.time"
+                :key="event.title"
+                :style="{ top: timeToY(event.time) + 'px', height: minutesToPixels(event.duration) + 'px' }"
+                class="my-event with-time"
+                @click="open(event)"
+                v-html="event.title"
+              ></div>
+            </template>
+          </template>
+        </v-calendar>
+      </v-sheet>
+    </v-col>
+  </v-row>
+        </v-list>
+      </v-card>
+    </v-dialog>
+  </v-row>
           </v-sheet>
-       
     </v-container>
   </v-app>
 </div>
@@ -42,8 +102,8 @@ export default {
   components: {
   
   },
-  data: () => ({
-   model: '',
+     data: () => ({
+           model: '',
     label: 'Bienvenido al administrador de turnos para Profesionales!',
     hint: 'Ingrese Legajo para administrar turnos',
     placeholder: '',
@@ -60,8 +120,34 @@ export default {
     counterEn: false,
     counter: 0,
     dense: false,
-  }),
-};
+        dialog: false,
+        notifications: false,
+        sound: true,
+        widgets: false,
+        today: new Date().getDay(),
+      events: [
+        {
+          name: 'Weekly Meeting',
+          start: '2019-01-07 09:00',
+          end: '2019-01-07 10:00',
+        },
+        {
+          name: 'Thomas\' Birthday',
+          start: '2019-01-10',
+        },
+        {
+          name: 'Mash Potatoes',
+          start: '2019-01-09 12:30',
+          end: '2019-01-09 15:30',
+        },
+        {
+          name: 'dsadasd ff',
+          start: '2019-10-25 13:30',
+          end: '2019-10-25 13:50'
+        }
+        ],
+    }),
+  };
 </script>
 
 <style scoped>
@@ -70,5 +156,30 @@ export default {
 }
 .v-label{
 font-size: 25px;
+}
+</style>
+
+<style scoped>
+.my-event {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  border-radius: 2px;
+  background-color: #1867c0;
+  color: #ffffff;
+  border: 1px solid #1867c0;
+  font-size: 12px;
+  padding: 3px;
+  cursor: pointer;
+  margin-bottom: 1px;
+  left: 4px;
+  margin-right: 8px;
+  position: relative;
+}
+
+.my-event.with-time {
+  position: absolute;
+  right: 4px;
+  margin-right: 0px;
 }
 </style>
--- src/components/pacientes.vue
+++ src/components/pacientes.vue
@@ -25,9 +25,70 @@
               :counter="counterEn ? counter : false"
               :dense="dense"
             ></v-text-field>
-            
+                     <v-row justify="center">
+    <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+      <template v-slot:activator="{ on }">
+        <v-btn depressed large color="primary" dark v-on="on">Ver Turnos</v-btn>
+      </template>
+      <v-card>
+        <v-toolbar dark color="primary">
+          <v-btn icon dark @click="dialog = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+          <v-toolbar-title>Próximos Turnos</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-toolbar-items>
+            <v-btn dark text @click="dialog = false">Save</v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
+        <v-list three-line subheader>
+           <v-row>
+    <v-col>
+      <v-sheet height="400">
+        <v-calendar
+          ref="calendar"
+          :now="today"
+          :value="today"
+          :events="events"
+          color="primary"
+          type="week"
+        >
+          <!-- the events at the top (all-day) -->
+          <template v-slot:day-header="{ date }">
+            <template v-for="event in eventsMap[date]">
+              <!-- all day events don't have time -->
+              <div
+                v-if="!event.time"
+                :key="event.title"
+                class="my-event"
+                @click="open(event)"
+                v-html="event.title"
+              ></div>
+            </template>
+          </template>
+          <!-- the events at the bottom (timed) -->
+          <template v-slot:day-body="{ date, timeToY, minutesToPixels }">
+            <template v-for="event in eventsMap[date]">
+              <!-- timed events -->
+              <div
+                v-if="event.time"
+                :key="event.title"
+                :style="{ top: timeToY(event.time) + 'px', height: minutesToPixels(event.duration) + 'px' }"
+                class="my-event with-time"
+                @click="open(event)"
+                v-html="event.title"
+              ></div>
+            </template>
+          </template>
+        </v-calendar>
+      </v-sheet>
+    </v-col>
+  </v-row>
+        </v-list>
+      </v-card>
+    </v-dialog>
+  </v-row>
           </v-sheet>
-       
     </v-container>
   </v-app>
 </div>
@@ -60,6 +121,33 @@ export default {
     counterEn: false,
     counter: 0,
     dense: false,
+    
+        dialog: false,
+        notifications: false,
+        sound: true,
+        widgets: false,
+        today: new Date().getDay(),
+      events: [
+        {
+          name: 'Weekly Meeting',
+          start: '2019-01-07 09:00',
+          end: '2019-01-07 10:00',
+        },
+        {
+          name: 'Thomas\' Birthday',
+          start: '2019-01-10',
+        },
+        {
+          name: 'Mash Potatoes',
+          start: '2019-01-09 12:30',
+          end: '2019-01-09 15:30',
+        },
+        {
+          name: 'dsadasd ff',
+          start: '2019-10-25 13:30',
+          end: '2019-10-25 13:50'
+        }
+        ],
   }),
 };
 </script>
